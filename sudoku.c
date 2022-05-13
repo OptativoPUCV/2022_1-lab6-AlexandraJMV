731df534807ca99cf62875862a98bed7a298d620
AlexandraJMV <>
2022-05-13 15:23:43 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -93,7 +93,7 @@ Node* DFS(Node* initial, int* cont){
 
 
 
-
+/*
 int main( int argc, char *argv[] ){
 
   Node* initial= read_file("s12a.txt");;
@@ -104,4 +104,4 @@ int main( int argc, char *argv[] ){
   print_node(final);
 
   return 0;
-}
+}*/
