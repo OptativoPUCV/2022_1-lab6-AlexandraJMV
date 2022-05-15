171d9206f2b06c1010a3ba08a95bc14006efcc8f
AlexandraJMV <>
2022-05-15 22:29:40 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -98,15 +98,16 @@ List* get_adj_nodes(Node* n){
     List* list=createList();
 
 	if (n == NULL) return NULL;
+
 	
 	int i, j;
-	for(int k = 1 ; k<=9 ; k++)
+	for(int k = 1 ; k<= 9 ; k++)
 	{
 		int comp = 0;
 		Node * cpy_node = copy(n);
 		for(i=0 ; i<9 ; i++)
 			for(j=0 ; j<9 ; j++)
-				if(cpy_node->sudo[i][j] == 0)
+				if(n->sudo[i][j] == 0)
 				{
 					cpy_node->sudo[i][j] = k;
 					comp=1;
